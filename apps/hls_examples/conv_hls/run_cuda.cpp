d68145bcdeb1a489c1f16ac38d6f7f67c185847f
Jing Pu <jimmypu07@gmail.com>
2016-03-09 16:41:43 -0800

    added opencv cuda benchmark

--- apps/hls_examples/conv_hls/run_cuda.cpp
+++ apps/hls_examples/conv_hls/run_cuda.cpp
@@ -10,7 +10,11 @@
 #include "pipeline_native.h"
 #include "pipeline_cuda.h"
 
+#include "opencv2/gpu/gpu.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
 using namespace Halide::Tools;
+using namespace cv;
 
 const unsigned char gaussian2d[5][5] = {
     {1,     3,     6,     3,     1},
@@ -47,13 +51,29 @@ int main(int argc, char **argv) {
     printf("CPU program runtime: %g\n", min_t * 1e3);
     save_image(out_native, "out_native.png");
 
+    /*
     // Timing code. Timing doesn't include copying the input data to
     // the gpu or copying the output back.
-    double min_t3 = benchmark(iter, 10, [&]() {
+    double min_t2 = benchmark(iter, 10, [&]() {
         pipeline_cuda(input, weight, out_cuda);
       });
-    printf("CUDA program runtime: %g\n", min_t3 * 1e3);
+    printf("Halide CUDA program runtime: %g\n", min_t2 * 1e3);
     save_image(out_cuda, "out_cuda.png");
+    */
+
+    Mat cv_input = imread(argv[1], CV_LOAD_IMAGE_COLOR);
+    Mat cv_input_rgba; // gpu::filter2D only support 1 or 4 channels
+    cvtColor(cv_input, cv_input_rgba, CV_BGR2RGBA, 4);
+    gpu::GpuMat d_input(cv_input_rgba);
+    gpu::GpuMat d_out(d_input.size(), d_input.type());
+    gpu::GpuMat d_out2(d_input.size(), d_input.type());
+
+    Mat kernel = getGaussianKernel(5, -1, CV_32F);
+    double min_t3 = benchmark(iter, 10, [&]() {
+            gpu::filter2D(d_input, d_out, -1, kernel);
+            gpu::filter2D(d_out, d_out2, -1, kernel);
+      });
+    printf("OpenCV CUDA program runtime: %g\n", min_t3 * 1e3);
 
     return 0;
 }
