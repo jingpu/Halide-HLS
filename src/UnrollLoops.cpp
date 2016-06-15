fc924681eaad1a891639213240e9776ffdb3b064
Jing Pu <jimmypu07@gmail.com>
2016-06-14 19:49:44 -0700

    reverted the changes in src/UnrollLoops.cpp

--- src/UnrollLoops.cpp
+++ src/UnrollLoops.cpp
@@ -7,38 +7,7 @@
 namespace Halide {
 namespace Internal {
 
-namespace {
-
-class ExpandExpr : public IRMutator {
-    using IRMutator::visit;
-    const Scope<Expr> &scope;
-
-    void visit(const Variable *var) {
-        if (scope.contains(var->name)) {
-            expr = scope.get(var->name);
-            debug(4) << "Fully expanded " << var->name << " -> " << expr << "\n";
-        } else {
-            expr = var;
-        }
-    }
-
-public:
-    ExpandExpr(const Scope<Expr> &s) : scope(s) {}
-
-};
-
-// Perform all the substitutions in a scope
-Expr expand_expr(Expr e, const Scope<Expr> &scope) {
-    ExpandExpr ee(scope);
-    Expr result = ee.mutate(e);
-    debug(4) << "Expanded " << e << " into " << result << "\n";
-    return result;
-}
-
-}
-
 class UnrollLoops : public IRMutator {
-    Scope<Expr> scope;
     using IRMutator::visit;
 
     void visit(const For *for_loop) {
@@ -58,8 +27,7 @@ class UnrollLoops : public IRMutator {
             Stmt block;
             // Make n copies of the body, each wrapped in a let that defines the loop var for that body
             for (int i = e->value-1; i >= 0; i--) {
-                Expr loop_var_value = simplify(expand_expr(for_loop->min + i, scope)); // use expanded expression to allow more potential simplifications
-                Stmt iter = substitute(for_loop->name, loop_var_value, body);
+                Stmt iter = substitute(for_loop->name, for_loop->min + i, body);
                 block = Block::make(iter, block);
             }
             stmt = block;
@@ -67,17 +35,6 @@ class UnrollLoops : public IRMutator {
         } else {
             IRMutator::visit(for_loop);
         }
-    }
-
-    void visit(const LetStmt *op) {
-        scope.push(op->name, simplify(expand_expr(op->value, scope)));
-        Stmt new_body = mutate(op->body);
-        if (new_body.same_as(op->body)) {
-            stmt = op;
-        } else {
-            stmt = LetStmt::make(op->name, op->value, new_body);
-        }
-        scope.pop(op->name);
     }
 };
 
