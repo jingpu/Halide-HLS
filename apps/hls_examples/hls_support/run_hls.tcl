6810fbe403f3e43045ab6ef87f71ffdea071ffa9
Jing Pu <jimmypu07@gmail.com>
2017-09-19 20:08:50 -0700

    Fix the vivado build error in #14.

--- apps/hls_examples/hls_support/run_hls.tcl
+++ apps/hls_examples/hls_support/run_hls.tcl
@@ -1,11 +1,12 @@
 set HALIDE_PATH $env(RUN_PATH)/../../..
+set HLS_INC_FLAGS "-I${HALIDE_PATH}/include -I$env(RUN_PATH)/../hls_support"
 set INC_FLAGS "-I${HALIDE_PATH}/include -I${HALIDE_PATH}/tools -I$env(RUN_PATH)/../../support -I$env(RUN_PATH)/../hls_support"
 set LD_FLAGS "$env(RUN_PATH)/pipeline_native.o -lpthread -ldl -lpng12"
 
 # creating the project and seting up the environtment
 open_project hls_prj
 set_top hls_target
-add_files hls_target.cpp -cflags "-std=c++0x -I../hls_support"
+add_files hls_target.cpp -cflags "-std=c++0x $HLS_INC_FLAGS"
 add_files -tb pipeline_hls.cpp -cflags "-std=c++0x $INC_FLAGS"
 add_files -tb run.cpp -cflags "-std=c++0x $INC_FLAGS"
 
