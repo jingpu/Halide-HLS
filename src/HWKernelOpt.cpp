f1943fe915817744c61a7fdad340a661ee78c637
CarlyleCao <caokd8888@outlook.com>
2017-10-12 23:46:51 +0800

    Add Feature for Reset Init in HW Optimization

--- src/HWKernelOpt.cpp
+++ src/HWKernelOpt.cpp
@@ -21,46 +21,127 @@ using std::set;
 using std::pair;
 using std::vector;
 
-namespace {
+struct InitInfo {
+    string op_name;
+    vector<Expr> op_values;
+};
 
-class ExpandExpr : public IRMutator {
+class ExtractInitInfo : public IRMutator {
+    using IRVisitor::visit;
+    
+    void visit(const Provide *op) {
+        init_info.op_name = op->name;
+        init_info.op_values = op->values;
+        IRMutator::visit(op);
+    }
+public:
+    InitInfo init_info;
+};
+
+InitInfo extract_init_info(Stmt s) {
+    ExtractInitInfo eii;
+    eii.mutate(s);
+    internal_assert(eii.init_info.op_values.size() == 1);
+    return eii.init_info;
+}
+
+class InsertInit : public IRMutator {
     using IRMutator::visit;
-    const Scope<Expr> &scope;
-
-    void visit(const Variable *var) {
-        if (scope.contains(var->name)) {
-            // xuan TODO check if remove likely_if_innermost is ok
-            //expr = scope.get(var->name);
-            expr = mutate(scope.get(var->name));
-            debug(4) << "Fully expanded " << var->name << " -> " << expr << "\n";
-        } else {
-            expr = var;
+    InitInfo init_info;
+    vector<Expr> op_args;
+    string hw_output_name;
+    string first_loop_var;
+    vector<Expr> hw_output_args;
+    
+    void visit(const Provide *op) {
+        hw_output_name = op->name;
+        hw_output_args = op->args;
+        vector<Expr> new_values(op->values.size());
+        for (size_t i = 0; i < op->values.size(); i++) {
+            Expr old_value = op->values[i];
+            Expr new_value = mutate(old_value);
+            new_values[i] = new_value;
         }
+        stmt = Provide::make("hw_output_acc", new_values, {0});
     }
 
     void visit(const Call *op) {
-        if (op->is_intrinsic(Call::likely) ||
-                   op->is_intrinsic(Call::likely_if_innermost)) {
-            assert(op->args.size() == 1);
-            expr = mutate(op->args[0]);
-        } else {
+        if(op->name == hw_output_name){
+            IRMutator::visit(op);
+            // TOOD: to be fixed
+            // expr = Call::make(Int(16), "hw_output_acc", {0}, Call::Halide);
+        }else{
             IRMutator::visit(op);
         }
     }
-public:
-    ExpandExpr(const Scope<Expr> &s) : scope(s) {}
 
+    void visit(const For *op) {
+        if(ends_with(op->name, "r$y")){
+            first_loop_var = op->name;
+        }
+        if(ends_with(op->name, "r$x")){
+            // First we alter the loop
+            Stmt new_body = mutate(op->body);
+            Stmt new_for_loop = For::make(op->name, op->min, op->extent, op->for_type, op->device_api, new_body);
+            // Initialize a variable to accumulate 
+            Expr hw_output_acc = Variable::make(Int(16), "hw_output_acc");
+            Stmt hw_output_acc_with_init = Provide::make("hw_output_acc", init_info.op_values, {0});
+            stmt = Block::make(hw_output_acc_with_init, new_for_loop);
+            // Add if else statement to utilize hw_output_acc
+            Stmt acc_provide = Provide::make(hw_output_name, {hw_output_acc}, hw_output_args);
+            Stmt else_provide = Provide::make(hw_output_name, {Add::make(hw_output_acc, Call::make(Int(16), hw_output_name, hw_output_args, Call::Halide))}, hw_output_args);
+            Expr condition_var = Variable::make(Int(32), first_loop_var);
+            Expr condition = EQ::make(condition_var, 0);
+            Stmt ifthen = IfThenElse::make(condition, acc_provide, else_provide);
+            stmt = Block::make(stmt, ifthen);
+        }else{
+            IRMutator::visit(op);    
+        }
+    }
+public:
+    void set_init_info(InitInfo ii){
+        init_info = ii;
+    }
 };
 
-// Perform all the substitutions in a scope
-Expr expand_expr(Expr e, const Scope<Expr> &scope) {
-    ExpandExpr ee(scope);
-    Expr result = ee.mutate(e);
-    debug(4) << "Expanded " << e << " into " << result << "\n";
-    return result;
+Stmt insert_init(Stmt s, InitInfo init_info){
+    InsertInit ini;
+    ini.set_init_info(init_info);
+    s = ini.mutate(s);
+    return s;
 }
 
-}
+class ResetInit : public IRMutator {
+    using IRMutator::visit;
+    
+    void visit(const ProducerConsumer *op) {    
+        if(op->is_producer){
+            Stmt body = op->body;
+            const Block *body_block = body.as<Block>();
+            // The following if finds the producer with update
+            if(body_block){
+                debug(0) << op->name << "\n";
+                debug(0) << "First: \n" << body_block->first << "\n";
+                debug(0) << "Rest: \n" << body_block->rest << "\n";
+                InitInfo init_info = extract_init_info(body_block->first);
+                body = insert_init(body_block->rest, init_info);
+            } else{
+                IRMutator::visit(op);
+            }
+            if (body.same_as(op->body)) {
+                stmt = op;
+            } else {
+                stmt = ProducerConsumer::make(op->name, op->is_producer, body);
+                debug(0) << "Modified: \n" << stmt << "\n";
+            }
+        }else {
+            IRMutator::visit(op);
+        }
+    }
+
+public:
+    /*ResetInit(const Scope<Expr> &s) : scope(s) {}*/
+};
 
 Stmt add_func_constraints(Stmt s, const HWKernelDAG &dag) {
 
@@ -99,7 +180,6 @@ class HWKernelOpt : public IRMutator {
 
     using IRMutator::visit;
    void visit(const ProducerConsumer *op) {
-
       if (starts_with(op->name, "output")){ //FIXME accelerate_at function name
           Stmt body = mutate(op->body);
           stmt = ProducerConsumer::make("_hls_target." + op->name, true, body);
@@ -116,12 +196,14 @@ public:
 
 Stmt hwkernel_opt(Stmt s, const HWKernelDAG &dag) {
     debug(3) << s << "\n";
+    // Now we find initiation before loop
+    s = ResetInit().mutate(s);
     s = add_func_constraints(s, dag);
     debug(3) << "after add output constraints:\n" << s << "\n";
     //s = push_init_into_update(s, dag);
     //debug(0) << "after push def into update:\n" << s << "\n";
     s = HWKernelOpt().mutate(s);
-    debug(3) << s << "\n";
+    debug(3) << "after kernel optimization:\n" << s << "\n";
     return s;
 }
 
