652cf892bf623f1f1ac26994b82e0587ab29a182
CarlyleCao <caokd8888@outlook.com>
2017-10-24 21:19:34 +0800

    Per cell version is now able to complie hls target

--- apps/hls_examples/lstm_hls/pipeline.cpp
+++ apps/hls_examples/lstm_hls/pipeline.cpp
@@ -13,7 +13,106 @@ using namespace Halide;
 // Vars
 Var x("x"), y("y"), z("z");
 
-class MyPipeline {
+class MyPipelinePerCell {
+public:
+    // Params
+    Param<int32_t> t;        // Time step
+    ImageParam Wxh;         // Weight from input to hidden, (NUM_INPUT, 4*NUM_HIDDEN)
+    ImageParam Whh;         // Recurrent weight of hidden, (NUM_HIDDEN, 4*NUM_HIDDEN)
+    ImageParam Why;         // Weight from Hidden to Output, (NUM_HIDDEN, NUM_OUTPUT)
+    ImageParam b;           // Bias, (4*NUM_HIDDEN, 1)
+    ImageParam input_t;     // Input at time t, (NUM_INPUT, BATCH_SIZE)
+    ImageParam h_tm1;       // Hiddens at time t-1, (NUM_HIDDEN, BATCH_SIZE)
+    ImageParam c_tm1;       // Cell states at time t-1, (NUM_HIDDEN, BATCH_SIZE)
+
+    // Reduction domain iterator
+    RDom rx;
+    RDom rh;
+    RDom ry;
+    RDom rb;
+
+    // Funcs
+    Func input_buf_copy;     // Buff copy for input
+    Func h_init;             // Initial state for h (4*NUM_HIDDEN, BATCH_SIZE)
+    Func c_init;             // Initial state for c, (4*NUM_HIDDEN, BATCH_SIZE)
+    Func h_t;                // Hiddens at time t, (NUM_HIDDEN, BATCH_SIZE)
+    Func c_t;                // Cell states at time t, (NUM_HIDDEN, BATCH_SIZE)
+    Func hw_output;          // Output at time t, (NUM_OUTPUT, BATCH_SIZE)
+    Func output;             // Output at time t, (NUM_OUTPUT, BATCH_SIZE)
+    Func pre_gate_ub;
+    Func pre_gate_t;
+    Func pre_gate_h;
+    Func gate[4];
+
+    std::vector<Argument> args;
+
+    MyPipelinePerCell():Wxh(Float(32),2), Whh(Float(32),2), Why(Float(32), 2), b(Float(32),2),
+        input_t(Float(32),2), h_tm1(Float(32),2), c_tm1(Float(32),2),
+        rx(0,NUM_INPUT), rh(0,NUM_HIDDEN), ry(0,NUM_OUTPUT), rb(0,BATCH_SIZE),
+        output("output")
+    {
+        // Matrix multiplication before gate
+        pre_gate_ub(x,y) = sum(input_t(rx,y) * Wxh(rx,x));
+        pre_gate_t(x,y) = pre_gate_ub(x,y) + b(x,0);
+        pre_gate_h(x,y) = sum(h_tm1(rh,y) * Whh(rh,x));
+        pre_gate_t(x,y) += pre_gate_h(x,y);
+        
+        // go through gates
+        gate[0](x,y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x,y)));
+        gate[1](x,y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x+NUM_HIDDEN,y)));
+        gate[2](x,y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x+2*NUM_HIDDEN,y)));
+        gate[3](x,y) = tanh(pre_gate_t(x+3*NUM_HIDDEN,y));
+
+        c_t(x,y) = gate[1](x,y) * c_tm1(x,y) + gate[0](x,y) * gate[3](x,y);
+        h_t(x,y) = gate[2](x,y) * tanh(c_t(x,y));
+        
+        hw_output(x, y) = sum(h_t(rh,y) * Why(rh,x));;
+        output(x, y) = hw_output(x, y);
+
+        // set bounds
+        input_t.dim(0).set_bounds(0, NUM_INPUT);
+        input_t.dim(1).set_bounds(0, BATCH_SIZE);
+        h_tm1.dim(0).set_bounds(0, NUM_HIDDEN);
+        h_tm1.dim(1).set_bounds(0, BATCH_SIZE);
+        c_tm1.dim(0).set_bounds(0, NUM_HIDDEN);
+        c_tm1.dim(1).set_bounds(0, BATCH_SIZE);
+        Wxh.dim(0).set_bounds(0, NUM_INPUT);
+        Wxh.dim(1).set_bounds(0, 4*NUM_HIDDEN);
+        Whh.dim(0).set_bounds(0, NUM_HIDDEN);
+        Whh.dim(1).set_bounds(0, 4*NUM_HIDDEN);
+        Why.dim(0).set_bounds(0, NUM_HIDDEN);
+        Why.dim(1).set_bounds(0, NUM_OUTPUT);
+        output.bound(x, 0, NUM_HIDDEN);
+        output.bound(y, 0, BATCH_SIZE);
+        
+        args = {input_t, h_tm1, c_tm1, Wxh, Whh, Why, b};
+    }
+
+    void compile_cpu() {
+        std::cout << "\ncompiling cpu code..." << std::endl;
+
+        output.compile_to_c("pipeline_lstm.cpp", args, "lstm");
+        output.compile_to_header("pipeline_native.h", args, "pipeline_native");
+        output.compile_to_object("pipeline_native.o", args, "pipeline_native");
+        output.print_loop_nest();
+    }
+    
+    void compile_hls() {
+        std::cout << "\ncompiling HLS code..." << std::endl;
+        
+        output.accelerate({input_t, h_tm1, c_tm1, Wxh, Whh, Why, b}, x, y);
+ 
+        Target hls_target = get_target_from_environment();
+        hls_target.set_feature(Target::CPlusPlusMangling);
+        output.print_loop_nest();
+        output.compile_to_lowered_stmt("pipeline_hls.ir.html", args, HTML, hls_target);
+        output.compile_to_hls("pipeline_hls.cpp", args, "pipeline_hls", hls_target);
+        output.compile_to_header("pipeline_hls.h", args, "pipeline_hls", hls_target); 
+    }
+    
+};
+
+class MyPipelinePerLayer {
 public:
     // Params
     ImageParam input;       // All inputs, (NUM_INPUT, BATCH_SIZE, T)
@@ -40,8 +139,8 @@ public:
     
     std::vector<Argument> args;
 
-    MyPipeline():input(Float(32),3),Wxh(Float(32),2),Whh(Float(32),2),b(Float(32),2),
-        rx(0,NUM_INPUT),rh(0,NUM_HIDDEN),ry(0,NUM_OUTPUT),rb(0,BATCH_SIZE),
+    MyPipelinePerLayer():input(Float(32),3), Wxh(Float(32),2), Whh(Float(32),2), b(Float(32),2),
+        rx(0,NUM_INPUT), rh(0,NUM_HIDDEN), ry(0,NUM_OUTPUT), rb(0,BATCH_SIZE),
         output("output")
     {
         // Initiation
@@ -69,25 +168,21 @@ public:
             pre_gate_t(x,y) = pre_gate_ub(x,y) + b(x,0);
             Func pre_gate_h;
             pre_gate_h(x,y) = sum(h_tm1(rh,y) * Whh(rh,x));
-            pre_gate_t(x, y) += pre_gate_h(x, y);
+            pre_gate_t(x,y) += pre_gate_h(x,y);
             
             // go through gates
             Func gate[4];
-            gate[0](x, y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x, y)));
-            if (t == 0) {
-                gate[1](x, y) = 0.0f;
-            } else {
-                gate[1](x, y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x+NUM_HIDDEN,y)));
-            }
-            gate[2](x, y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x+2*NUM_HIDDEN,y)));
-            gate[3](x, y) = tanh(pre_gate_t(x+3*NUM_HIDDEN,y));
+            gate[0](x,y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x,y)));
+            gate[1](x,y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x+NUM_HIDDEN,y)));
+            gate[2](x,y) = 1.0f / (1.0f + fast_exp(-pre_gate_t(x+2*NUM_HIDDEN,y)));
+            gate[3](x,y) = tanh(pre_gate_t(x+3*NUM_HIDDEN,y));
 
-            c[t](x, y) = gate[1](x, y) * c_tm1(x, y) + gate[0](x, y) * gate[3](x, y);
-            h[t](x, y) = gate[2](x, y) * tanh(c[t](x, y));
+            c[t](x,y) = gate[1](x,y) * c_tm1(x,y) + gate[0](x,y) * gate[3](x,y);
+            h[t](x,y) = gate[2](x,y) * tanh(c[t](x, y));
         }
         output(x, y, z) = 0.0f;
         for (int t = 0; t < T; t++) {
-            output(x, y, t) = h[t](x, y);
+            output(x,y,t) = h[t](x,y);
         }
 
         // set bounds
@@ -114,6 +209,7 @@ public:
         output.print_loop_nest();
     }
     
+    /*
     void compile_hls() {
         std::cout << "\ncompiling HLS code..." << std::endl;
         
@@ -126,16 +222,16 @@ public:
         output.compile_to_hls("pipeline_hls.cpp", args, "pipeline_hls", hls_target);
         output.compile_to_header("pipeline_hls.h", args, "pipeline_hls", hls_target); 
     }
-    
+    */    
 };
 
 int main(int argc, char **argv) {
   
-    MyPipeline p1;
+    MyPipelinePerCell p1;
     p1.compile_cpu();
     
-    MyPipeline p2;
+    MyPipelinePerCell p2;
     p2.compile_hls();
-    
+
     return 0;
 }
