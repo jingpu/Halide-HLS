867502eb9ba9241c82b5048e696fbcd34d088c49
CarlyleCao <caokd8888@outlook.com>
2017-10-26 00:54:39 +0800

    push on lstm testbench

--- apps/hls_examples/lstm_hls/lstm_test.cpp
+++ apps/hls_examples/lstm_hls/lstm_test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 #include "hls_target.h"
 
 #define HW_COSIM
@@ -45,19 +46,27 @@ int main(int argc, char **argv)
         b[i] = i;
     }
 
-    for(int i = 0; i < T; i++){
-        for(int j = 0; j < BATCH_SIZE; j++){
-            for(int k = 0; k < NUM_INPUT; k++){
-                Input[i * BATCH_SIZE * NUM_INPUT + j * NUM_INPUT + k] = i + j + k;
+    for(int t = 0; t < T; t++){
+        for(int i = 0; i < BATCH_SIZE; i++){
+            for(int j = 0; j < NUM_INPUT; j++){
+                Input[t * BATCH_SIZE * NUM_INPUT + i * NUM_INPUT + j] = t + i + j;
             }
         }
     }
 
     static Mat_t hw_result[NUM_OUTPUT * BATCH_SIZE * T];
-    for(int i = 0; i < T; i++) {
-        for(int j = 0; j < BATCH_SIZE; j++) {
-            for(int k = 0; k < NUM_OUTPUT; k++){
-                hw_result[ * BATCH_SIZE * NUM_OUTPUT + j * NUM_OUTPUT + k] = 0;
+    for(int t = 0; t < T; t++) {
+        for(int i = 0; i < BATCH_SIZE; i++) {
+            for(int j = 0; j < NUM_OUTPUT; j++){
+                hw_result[t * BATCH_SIZE * NUM_OUTPUT + i * NUM_OUTPUT + j] = 0;
+            }
+        }
+    }
+    static Mat_t sw_result[NUM_OUTPUT * BATCH_SIZE * T];
+    for(int t = 0; t < T; t++) {
+        for(int i = 0; i < BATCH_SIZE; i++) {
+            for(int j = 0; j < NUM_OUTPUT; j++){
+                sw_result[t * BATCH_SIZE * NUM_OUTPUT + i * NUM_OUTPUT + j] = 0;
             }
         }
     }
@@ -65,24 +74,53 @@ int main(int argc, char **argv)
 
     static Mat_t sw_result[NUM_OUTPUT * BATCH_SIZE * T];
     // Generate the expected result
-    static Mat_t h_tm1(NUM_HIDDEN * BATCH_SIZE);
-    static Mat_t c_tm1(NUM_HIDDEN * BATCH_SIZE);
-    static Mat_t h_t(NUM_HIDDEN * BATCH_SIZE);
-    static Mat_t c_t(NUM_HIDDEN * BATCH_SIZE);
+    static Mat_t h_tm1[NUM_HIDDEN * BATCH_SIZE];
+    static Mat_t c_tm1[NUM_HIDDEN * BATCH_SIZE];
+    static Mat_t h_t[NUM_HIDDEN * BATCH_SIZE];
+    static Mat_t c_t[NUM_HIDDEN * BATCH_SIZE];
     for(int i = 0; i < BATCH_SIZE; i++){
         for(int j = 0; j < NUM_HIDDEN; j++){
-            h_tm1(i * NUM_HIDDEN + j) = 0;
+            h_tm1[i * NUM_HIDDEN + j] = 0;
         }
     }
     for(int i = 0; i < BATCH_SIZE; i++){
         for(int j = 0; j < NUM_HIDDEN; j++){
-            c_tm1(i * NUM_HIDDEN + j) = 0;
+            c_tm1[i * NUM_HIDDEN + j] = 0;
         }
     }
-    for(int i = 0; i < T; i++) {
+    for(int t = 0; t < T; t++) {
+
+        // Matrix multiplication before gates
+        Mat_t pregate_t[4 * NUM_HIDDEN * BATCH_SIZE];
+        for(int i = 0; i < BATCH_SIZE; i++){
+            for(int j = 0; j < 4 * NUM_HIDDEN; j++){
+                pregate_t[t * BATCH_SIZE * 4 * NUM_HIDDEN + j * 4 * NUM_HIDDEN + k] = 0;
+                for(int k = 0; k < NUM_INPUT; k++){
+                    pregate_t[i * 4 * NUM_HIDDEN + j] += Input[t * BATCH_SIZE * NUM_INPUT + i * NUM_INPUT + k] 
+                        * Wxh[j * NUM_INPUT + k] + b[j];
+                }
+            }
+        }
+        Mat_t pregate_h[4 * NUM_HIDDEN * BATCH_SIZE];
+        for(int i = 0; i < BATCH_SIZE; i++){
+            for(int j = 0; j < 4 * NUM_HIDDEN; j++){
+                pregate_h[t * BATCH_SIZE * 4 * NUM_HIDDEN + j * 4 * NUM_HIDDEN + k] = 0;
+                for(int k = 0; k < NUM_HIDDEN; k++){
+                    pregate_h[i * 4 * NUM_HIDDEN + j] += h_tm1[t * BATCH_SIZE * NUM_INPUT + i * NUM_INPUT + k] 
+                        * Whh[j * NUM_HIDDEN + k];
+                }
+            }
+        }
+        for(int i = 0; i < BATCH_SIZE; i++){
+            for(int j = 0; j < 4 * NUM_HIDDEN; j++){
+                pregate_t[i * 4 * NUM_HIDDEN + j] += pregate_h[i * 4 * NUM_HIDDEN + j]；
+            }
+        }
+
+        // Go through all the gates
     }
 
-
+/*
 #ifdef HW_COSIM
     hls_target(hw_result, in_mat_a, in_mat_b, 64, 384); 
 #endif
@@ -110,6 +148,7 @@ int main(int argc, char **argv)
     else
         cout << "Test passes." << endl;
 #endif
-    return err_cnt;
+*/
+    return 0;
 }
 
